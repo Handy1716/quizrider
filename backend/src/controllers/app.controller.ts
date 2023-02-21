54e6b448b845491c4a1ff184eca49213b1348a31
Polgár Kristóf <polgarkristof10@gmail.com>
2023-02-21 20:19:00 +0100

    database

--- backend/src/controllers/app.controller.ts
+++ backend/src/controllers/app.controller.ts
@@ -1,13 +1,10 @@
 import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
-import { DataSource } from "typeorm";
-import './app.datasource';
+import { AppService } from '../services/app.service';
 
 @Controller()
 export class AppController {
   constructor(
-    private readonly appService: AppService,
-    private dataSource: DataSource
+    private readonly appService: AppService
   ) {}
 
   @Get()
