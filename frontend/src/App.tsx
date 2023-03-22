7535eaaa0e85c5e8b4ea00415c3b99ea5b133242
Polgár Kristóf <polgarkristof10@gmail.com>
2023-03-22 16:42:36 +0100

    state

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -8,7 +8,16 @@ import Header from './Components/header';
 import { Col, Container, Row, Button } from 'react-bootstrap';
 
 function App() {
+
+// this.state = {
+//   logged: false,
+//   page: "main"
+// }
+  // state.logged = true/false - be van-e lepve
+  // state.page = "login","main","signup"
+
   return (
+    <Header page={this.state.page}/>
     <Container>
       <Register />
     </Container>
