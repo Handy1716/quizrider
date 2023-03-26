4c4919aaf449887ab683c1ae1b0606e68867ea76
Polgár Kristóf <polgarkristof10@gmail.com>
2023-03-26 16:23:55 +0200

    main menu, swiching between login, register

--- frontend/src/Components/header.tsx
+++ frontend/src/Components/header.tsx
@@ -1,10 +1,17 @@
 import { Button } from "react-bootstrap";
 
+export default function Header({page, loginClick, registerClick} : {page : string, loggedIn: boolean, loginClick:() => void, registerClick:() => void}) {
 
-export default function Header({value, onHeaderClick}:{value:string, onHeaderClick:()=>void}) {
-    return(
-    <>
-    <Button className="btn" onClick={onHeaderClick}>{value}</Button>
-    </>
+    // login gomb akkor kell, ha a register oldalon vagyok
+    const loginVisible = (page === "register");
+    // signup gomb akkor kell, ha a login oldalon vagyok
+    const signupVisible = (page === "login");
+
+    return (
+        <>
+        <img src="" alt="" />
+        <Button className={`btn ${loginVisible ? 'd-inline-block' : 'd-none'}`} onClick={loginClick}>Login</Button>
+        <Button className={`btn ${signupVisible ? 'd-inline-block' : 'd-none'}`} onClick={registerClick}>Signup</Button>
+        </>
     )
 }
