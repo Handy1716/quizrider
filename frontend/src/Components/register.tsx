0afaa8e4571a0e457ebe89213b0da3674ffe3fce
Polgár Kristóf <polgarkristof10@gmail.com>
2023-03-29 17:48:39 +0200

    REGISTER FORM

--- frontend/src/Components/register.tsx
+++ frontend/src/Components/register.tsx
@@ -1,22 +1,29 @@
-import { Button, Col, Row } from "react-bootstrap";
+import { Button, Col, Form, Row } from "react-bootstrap";
 export default function Register() {
     return (
         <>
-            <form action="/creator" /* ezen kell javitani */ method="post">
-                <Row>
-                    <Col><h3 className="logRegText">Email:</h3></Col>
-                    <Col><input name="email" id="email" type="text" /></Col>
-                </Row>
-                <Row>
-                    <Col><h3 className="logRegText">Password:</h3></Col>
-                    <Col><input name="password" id="password"></input></Col>            
-                </Row>
-                <Row>
-                    <Col><h3 className="logRegText">Password again:</h3></Col>
-                    <Col><input name="password2" id="password2" /></Col>            
-                </Row>
-                <Row><Col className="centering"><Button type="submit">Register</Button></Col></Row>
-            </form>
+            <Form>
+            <Form.Group className="mb-3" controlId="loginFormUser">
+                <Form.Label>User name:</Form.Label>
+                <Form.Control type="text" name="name" required placeholder="Jhon Doe" />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="loginFormEmail">
+                <Form.Label>Email address</Form.Label>
+                <Form.Control type="email" name="email" required placeholder="jhon.doe@example.hu" />
+            </Form.Group>
+    
+            <Form.Group className="mb-3" controlId="loginFormPassword">
+                <Form.Label>Password</Form.Label>
+                <Form.Control type="password" name="password" required placeholder="Jhon1996" />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="loginFormPassword">
+                <Form.Label>Password again:</Form.Label>
+                <Form.Control type="password" name="password" />
+            </Form.Group>
+            <Button variant="primary" type="submit" className="mb-3">
+                Login
+            </Button>
+        </Form>
         </>
     )
 }
