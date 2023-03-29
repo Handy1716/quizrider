55d604c956b1fc6a096fa2754562f15722cf8251
Polgár Kristóf <polgarkristof10@gmail.com>
2023-03-29 18:53:48 +0200

    tags

--- frontend/src/Components/createQuiz.tsx
+++ frontend/src/Components/createQuiz.tsx
@@ -1,52 +1,24 @@
 import { Button, Col, Form, Row } from "react-bootstrap";
+import Tags from "./tags";
 //<div class="form-check form-switch"><input type="checkbox" id="custom-switch" class="form-check-input"><label title="" for="custom-switch" class="form-check-label">Check this switch</label></div>
 
 export default function CreateQuiz(){
-    return(
-        <div>
-            <Row>
-                <Col>
-                    <form>
-                         <Row>
-                            <Col><h4 className="logRegText">Quiz name:</h4></Col>
-                            <Col><input name="email" id="email" type="text" /></Col>
-                        </Row>
-                        <form>
-                        <Row>
-                            
-                            <Col><h4 className="logRegText">Add your quiz tag:</h4></Col>
-                            <Col><input name="email" id="email" type="text" /><Button type="submit">+</Button></Col>
-                                        
-                        </Row>
-                        </form>
-                        <Row>
-                            <Col><h4 className="logRegText">One round:</h4></Col>
-                            <Col>
-                            <Form.Check 
-                            type="switch"
-                            id="custom-switch"
-                            label=""
-                            />
-                            </Col>
-                        </Row>
-                        <Row>
-                            <Col><h4 className="logRegText">Private:</h4></Col>
-                            <Col>
-                            <Form.Check 
-                            type="switch"
-                            id="custom-switch"
-                            label=""
-                            />
-                            </Col>
-                        </Row>
-                        <Row>
-                        </Row>
-                    </form>
-                </Col>
-                aas
-                <Col/>  
-            </Row>
-        </div>
+    return(<>
+    <Tags />
+        <Form>
+            <Form.Group>
+                <Form.Label>Quiz name:</Form.Label>
+                <Form.Control type="text" name="name" required placeholder="My best quiz"/>
+            </Form.Group>
+            
+            <Form.Group>
+                <Form.Label></Form.Label>
+                <Form.Control />
+            </Form.Group>
+            <Form.Group>
+                <Form.Label></Form.Label>
+                <Form.Control></Form.Control>
+            </Form.Group>
+        </Form></>
     )
-
 }
--- frontend/src/Components/tags.tsx
+++ frontend/src/Components/tags.tsx
@@ -1,7 +1,26 @@
+import { useState } from "react"
+import { Button } from "react-bootstrap"
+import { InputTags } from "react-bootstrap-tagsinput"
 
 
 export default function Tags() {
+    const [state, setState] = useState<string[]>([])
+
+
     return(
-        <div></div>
+        <div>
+      <div className='input-group'>
+        <InputTags values={state} onTags={(value) => setState(value.values)} />
+        <Button
+          type='button'
+          data-testid='button-clearAll'
+          onClick={() => {
+            setState([])
+          }}
+        >
+          Clear
+        </Button>
+      </div>
+    </div>
     )
 }
