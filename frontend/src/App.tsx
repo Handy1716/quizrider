91a8c3aa48b5bbff755a9bbf974903c27dad3887
Polgár Kristóf <polgarkristof10@gmail.com>
2023-04-04 18:42:36 +0200

    a

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -49,7 +49,7 @@ function App() {
     <Header page={state.page} loggedIn={state.loggedIn} loginClick={loginClick} registerClick={registerClick} mainClick={mainClick}/>
     <div className='background'>
     <br />
-      <div className='main'>
+      <div className=''>
       {state.loggedIn===false && state.page==="main" &&(<Main loginClick={loginClick} registerClick={registerClick}/>)}
       {state.loggedIn===false && state.page==="login" &&(<Login />)}
       {state.loggedIn===false && state.page==="register" &&(<Register />)}
