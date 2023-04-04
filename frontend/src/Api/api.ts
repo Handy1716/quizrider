66d03d387ec4c678aed2492adf0cd779ea063b62
Polgár Kristóf <polgarkristof10@gmail.com>
2023-04-04 13:12:31 +0200

    api

--- frontend/src/Api/api.ts
+++ frontend/src/Api/api.ts
@@ -1,5 +1,25 @@
+import { getToken } from "./session";
 
 const BACKEND_URL = "http://localhost:3001"
+
+export function apiCreator(data : any, responseCallback : Function, errorCallback : Function = () => {}): any
+{
+    fetch(BACKEND_URL + '/creator', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(Object.fromEntries(data)),
+    })
+    .then(response => {
+        if (response.ok) {
+            return response.json();
+        }
+        throw new Error(response.statusText);
+    })
+    .then(data => responseCallback(data))
+    .catch(error => errorCallback(error));
+}
 
 export function apiLogin(data : any, responseCallback : Function, errorCallback : Function = () => {}): any
 {
@@ -11,8 +31,197 @@ export function apiLogin(data : any, responseCallback : Function, errorCallback 
         body: JSON.stringify(Object.fromEntries(data)),
     })
     .then(response => {
-        if (response.ok)
+        if (response.ok) {
             return response.json();
+        }
+        throw new Error(response.statusText);
+    })
+    .then(data => responseCallback(data))
+    .catch(error => errorCallback(error));
+}
+
+export function apiCreatorMe(responseCallback : Function, errorCallback : Function = () => {}): any
+{
+    fetch(BACKEND_URL + '/creator/me', {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer ' + getToken()
+        }
+    })
+    .then(response => {
+        if (response.ok) {
+            return response.json();
+        }
+        throw new Error(response.statusText);
+    })
+    .then(data => responseCallback(data))
+    .catch(error => errorCallback(error));
+}
+
+export function apiQuizMe(responseCallback : Function, errorCallback : Function = () => {}): any
+{
+    fetch(BACKEND_URL + '/quiz/me', {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer ' + getToken()
+        }
+    })
+    .then(response => {
+        if (response.ok) {
+            return response.json();
+        }
+        throw new Error(response.statusText);
+    })
+    .then(data => responseCallback(data))
+    .catch(error => errorCallback(error));
+}
+
+export function apiQuizPublic(responseCallback : Function, errorCallback : Function = () => {}): any
+{
+    fetch(BACKEND_URL + '/quiz/public', {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+        }
+    })
+    .then(response => {
+        if (response.ok) {
+            return response.json();
+        }
+        throw new Error(response.statusText);
+    })
+    .then(data => responseCallback(data))
+    .catch(error => errorCallback(error));
+}
+
+export function apiQuizGet(id : number, responseCallback : Function, errorCallback : Function = () => {}): any
+{
+    fetch(BACKEND_URL + '/quiz/' + id, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer ' + getToken()
+        }
+    })
+    .then(response => {
+        if (response.ok) {
+            return response.json();
+        }
+        throw new Error(response.statusText);
+    })
+    .then(data => responseCallback(data))
+    .catch(error => errorCallback(error));
+}
+
+export function apiQuizSet(data : any, responseCallback : Function, errorCallback : Function = () => {}): any
+{
+    fetch(BACKEND_URL + '/quiz', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer ' + getToken()
+        },
+        body: JSON.stringify(Object.fromEntries(data)),
+    })
+    .then(response => {
+        if (response.ok) {
+            return response.json();
+        }
+        throw new Error(response.statusText);
+    })
+    .then(data => responseCallback(data))
+    .catch(error => errorCallback(error));
+}
+
+export function apiQuizDel(id : number, responseCallback : Function, errorCallback : Function = () => {}): any
+{
+    fetch(BACKEND_URL + '/quiz/' + id, {
+        method: 'DELETE',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer ' + getToken()
+        }
+    })
+    .then(response => {
+        if (response.ok) {
+            return response.json();
+        }
+        throw new Error(response.statusText);
+    })
+    .then(data => responseCallback(data))
+    .catch(error => errorCallback(error));
+}
+
+export function apiRuncode(quizId : number, responseCallback : Function, errorCallback : Function = () => {}): any
+{
+    fetch(BACKEND_URL + '/runcode/' + quizId, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer ' + getToken()
+        }
+    })
+    .then(response => {
+        if (response.ok) {
+            return response.json();
+        }
+        throw new Error(response.statusText);
+    })
+    .then(data => responseCallback(data))
+    .catch(error => errorCallback(error));
+}
+
+export function apiPlay(runcode : string, responseCallback : Function, errorCallback : Function = () => {}): any
+{
+    fetch(BACKEND_URL + '/play/' + runcode, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+        }
+    })
+    .then(response => {
+        if (response.ok) {
+            return response.json();
+        }
+        throw new Error(response.statusText);
+    })
+    .then(data => responseCallback(data))
+    .catch(error => errorCallback(error));
+}
+
+export function apiTagTop(responseCallback : Function, errorCallback : Function = () => {}): any
+{
+    fetch(BACKEND_URL + '/tag/top', {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+        }
+    })
+    .then(response => {
+        if (response.ok) {
+            return response.json();
+        }
+        throw new Error(response.statusText);
+    })
+    .then(data => responseCallback(data))
+    .catch(error => errorCallback(error));
+}
+
+export function apiScoreboard(data : any, responseCallback : Function, errorCallback : Function = () => {}): any
+{
+    fetch(BACKEND_URL + '/scoreboard', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(Object.fromEntries(data)),
+    })
+    .then(response => {
+        if (response.ok) {
+            return response.json();
+        }
         throw new Error(response.statusText);
     })
     .then(data => responseCallback(data))
---
+++ frontend/src/Api/session.ts
@@ -0,0 +1,17 @@
+
+export function getDevice() {
+    let deviceId = localStorage.getItem('device');
+    if (!deviceId) {
+        deviceId = crypto.randomUUID();
+        localStorage.setItem('device', deviceId);
+    }
+    return deviceId;
+}
+
+export function setToken(token : string) {
+    localStorage.setItem('token', token);
+}
+
+export function getToken() {
+    return localStorage.getItem('token');
+}
