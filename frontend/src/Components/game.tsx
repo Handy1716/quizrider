bd9eb94388955aea08f45fa477e94ec8b0aca8ad
Polgár Kristóf <polgarkristof10@gmail.com>
2023-04-12 12:58:13 +0200

    game

--- frontend/src/Components/game.tsx
+++ frontend/src/Components/game.tsx
@@ -1,13 +1,30 @@
+import React, { useState } from 'react';
 import { FORMERR } from "dns";
 import { Button, Col, Container, Form, Row } from "react-bootstrap";
 
 
-export default function Game(){
+export default function Game(quiz: any){
+    const [round, setRound] = useState<number>(0); 
+    function btn1(){
+        if(quiz.quiz.questions[round].answers[0].rightAnswer==true){
+            //  TODO: 2 másodpercig legyen zöld és ne legyen kattintható és számolódjon valahol hogy sikeres pontja volt             
+        } else {
+            // TODO: 2 másodpercig legyen piros és ne legyen kattintható
+        }
+        if(quiz.quiz.questions[round+1]!=quiz.quiz.questions.length){
+            // TODO: rendelelje le a scoreboardot vagy a végét a Game ne látszódjon 
+        } else {
+            setRound(round+1);
+        }
+    }
+    console.log('ezitt', quiz);
     return(
+        
         <Form>
+        {quiz.quiz.name}
             <div className="main centering question">Kerdes</div>
-            <Row className="answerRow"><Col ><div className="answer">asd</div></Col><Col><div className="answer">asd</div></Col></Row>
-            <Row className="answerRow"><Col><div className="answer">asd</div></Col><Col><div className="answer">ask</div></Col></Row>
+            <Row className="answerRow"><Col ><div className="answer" onClick={btn1}>{quiz.quiz.questions.length}</div></Col><Col><div className="answer" onClick={btn1}></div></Col></Row>
+            <Row className="answerRow"><Col><div className="answer" onClick={btn1}></div></Col><Col><div className="answer" onClick={btn1}></div></Col></Row>
         </Form>
     )
 }
